0cda47af4dc1651f590694754ab656a0586af1d3
Kvikipedia <viktor.eftimov1@gmail.com>
2020-12-28 11:36:16 +0100

    ParkEasy

--- app/src/main/java/com/example/parkeazy/ReservationFormActivity.java
+++ app/src/main/java/com/example/parkeazy/ReservationFormActivity.java
@@ -73,7 +73,7 @@ public class ReservationFormActivity extends AppCompatActivity implements Adapte
             datePickerDialog = new DatePickerDialog(ReservationFormActivity.this,
                     new DatePickerDialog.OnDateSetListener() {
                         @Override
-                        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        public void onDateSet(DatePicker view, int year, int month, int day) {
                             date_picker.setText(day + "/" + (month + 1) + "/" + year);
                         }
                     }, year, month, day);
